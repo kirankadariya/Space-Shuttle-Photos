4b21e84d2fcbc7a313db0bca6b40d08100708d9a
Kiran Kadariya <kirankadariya@gmail.com>
2023-12-24 19:04:28 -0600

    done

--- netlify/functions/apodApi/apodApi.js
+++ netlify/functions/apodApi/apodApi.js
@@ -1,7 +1,7 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {   const ApiKEY = process.env.apiKey;
-    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=${apiKEY}');
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=${ApiKEY}');
     const data = await response.json();
  
 
