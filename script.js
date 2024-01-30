4464987673a5b858f697d2fec2c798435e9ee337
kirankadariya <128102618+kirankadariya@users.noreply.github.com>
2024-01-30 17:32:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const errorMsg = document.querySelector(".error-msg");
 const dateInput = document.querySelector("#date-input");
 const submitButton = document.querySelector("#submit-button");
 
-const apodURL = "https://space-shuttle-mar.netlify.app/functions/apodApi";
+const apodURL = "https://space-shuttle-mar/.netlify.app/functions/apodApi";
 
 async function fetchData(url, handleData) {
   try {
@@ -56,7 +56,7 @@ submitButton.addEventListener("click", function () {
     ? dateInput.value
     : new Date().toISOString().slice(0, 10);
 
-    const roverURL = `https://space-shuttle-mar.netlify.app/functions/roverApi?date=${date}`;
+    const roverURL = `https://space-shuttle-mar/.netlify.app/functions/roverApi?date=${date}`;
 
   fetchData(roverURL, handleRoverData);
 });
